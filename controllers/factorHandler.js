b60947edf9b47a14848614779fb5f99a9d14c39a
G M Ganesh <gmganesh173@gmail.com>
2023-10-28 20:38:35 +0530

    api code changes

--- controllers/factorHandler.js
+++ controllers/factorHandler.js
@@ -22,7 +22,7 @@ exports.updateOne = (Model) =>
         if (!doc) {
             return next(new AppError(`No doc found`, 404));
         }
-        res.status(201).json({
+        res.status(200).json({
             status: "success",
             data: doc,
         });
@@ -33,7 +33,7 @@ exports.updateOne = (Model) =>
 exports.createOne = (Model) =>
     catchAsync(async (req, res, next) => {
         const data = await Model.create(req.body);
-        res.status(200).json({
+        res.status(201).json({
             status: "success",
             data,
         });
